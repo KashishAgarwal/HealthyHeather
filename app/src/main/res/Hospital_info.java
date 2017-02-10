965afd11a16cfeb18154194b504813d935936b80
KashishAgarwal <agarwalkashish.123@gmail.com>
2017-02-11 00:29:21 +0530

    Data Class created

--- app/src/main/res/Hospital_info.java
+++ app/src/main/res/Hospital_info.java
@@ -5,5 +5,16 @@
 public class Hospital_info {
 
     private int  id;
-    
+    private  String loc_cor;
+    private String location;
+    private  String name;
+    private String med_type;
+    private String pin;
+    private String state;
+    private String tel;
+    private String mobile;
+    private String emer_mob;
+    private String email;
+    private String spec;
+    private String district;
 }
