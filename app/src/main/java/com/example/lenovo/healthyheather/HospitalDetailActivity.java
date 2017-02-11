15ee2ef92b194b4b1e6c608fb926757d6adba275
KashishAgarwal <agarwalkashish.123@gmail.com>
2017-02-11 17:05:57 +0530

    Implemented hospital details activity

--- app/src/main/java/com/example/lenovo/healthyheather/HospitalDetailActivity.java
+++ app/src/main/java/com/example/lenovo/healthyheather/HospitalDetailActivity.java
@@ -3,16 +3,27 @@ package com.example.lenovo.healthyheather;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HospitalDetailActivity extends AppCompatActivity {
 
-    HospitalInfo curHospital=null;
+    HospitalInfo a=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospital_detail);
         Intent i=getIntent();
-        curHospital= (HospitalInfo) i.getSerializableExtra("hospital");
+        a= (HospitalInfo) i.getSerializableExtra("hospital");
+
+        TextView Name= (TextView) findViewById(R.id.T1);
+        TextView Add= (TextView) findViewById(R.id.T2);
+        TextView Spec= (TextView) findViewById(R.id.T3);
+
+
+
+        Name.setText(a.Hospital_Name);
+        Add.setText(a.Location);
+        Spec.setText(a.Specialties);
 
     }
 }
--- app/src/main/java/com/example/lenovo/healthyheather/Navigate.java
+++ app/src/main/java/com/example/lenovo/healthyheather/Navigate.java
@@ -1,27 +1,44 @@
 package com.example.lenovo.healthyheather;
 
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Navigate extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        int a = getExtraData();
         setContentView(R.layout.activity_navigate);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -40,9 +57,68 @@ public class Navigate extends FragmentActivity implements OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         LatLng curr = new LatLng(MainActivity.latitude, MainActivity.longitude);
-        mMap.addMarker(new MarkerOptions().position(curr).title("You are here!").alpha(0.7f));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr,15));
 
-        
+        final Marker marker = mMap.addMarker(new MarkerOptions().position(curr).title("You are here!").alpha(0.7f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(curr, 1));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng point) {
+
+                // Already two locations
+
+                String des = MainActivity.completeList.get(a).Location_Coordinates;
+                String pos[] = des.split(",");
+                LatLng dest = new LatLng(Double.parseDouble(pos[0]), Double.parseDouble(pos[1]));
+
+                // Getting URL to the Google Directions API
+                String url = getUrl(origin, dest);
+                Log.d("onMapClick", url.toString());
+                FetchUrl FetchUrl = new FetchUrl();
+
+                // Start downloading json data from Google Directions API
+                FetchUrl.execute(url);
+                //move map camera
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(origin));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+            }
+
+        }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Navigate Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
-}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
